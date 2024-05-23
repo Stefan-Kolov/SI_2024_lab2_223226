bf1ba1276c67386f5fec26c6b7a959cb28d180a2
Stefan Kolov <kolov723@gmail.com>
2024-05-23 23:13:39 +0200

    Tests Added

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -49,45 +49,45 @@ class Item {
 
 public class SILab2 {
     public static boolean checkCart(List<Item> allItems, int payment){
-        if (allItems == null){
-            throw new RuntimeException("allItems list can't be null!");
+        if (allItems == null){//1
+            throw new RuntimeException("allItems list can't be null!");//2
         }
 
-        float sum = 0;
+        float sum = 0;//3
 
-        for (int i = 0; i < allItems.size(); i++){
-            Item item = allItems.get(i);
-            if (item.getName() == null || item.getName().length() == 0){
-                item.setName("unknown");
+        for (int i = 0; i < allItems.size(); i++){//4
+            Item item = allItems.get(i);//5
+            if (item.getName() == null || item.getName().length() == 0){//6
+                item.setName("unknown");//7
             }
-            if (item.getBarcode() != null){
-                String allowed = "0123456789";
-                char chars[] = item.getBarcode().toCharArray();
-                for (int j = 0; j < item.getBarcode().length(); j++){
-                    char c = item.getBarcode().charAt(j);
-                    if (allowed.indexOf(c) == -1){
-                        throw new RuntimeException("Invalid character in item barcode!");
+            if (item.getBarcode() != null){//8
+                String allowed = "0123456789";//9
+                char chars[] = item.getBarcode().toCharArray();//10
+                for (int j = 0; j < item.getBarcode().length(); j++){//11
+                    char c = item.getBarcode().charAt(j);//12
+                    if (allowed.indexOf(c) == -1){//13
+                        throw new RuntimeException("Invalid character in item barcode!");//14
                     }
                 }
-                if (item.getDiscount() > 0){
-                    sum += item.getPrice()*item.getDiscount();
+                if (item.getDiscount() > 0){//15
+                    sum += item.getPrice()*item.getDiscount();//16
                 }
-                else {
-                    sum += item.getPrice();
+                else {//17
+                    sum += item.getPrice();//18
                 }
             }
-            else {
-                throw new RuntimeException("No barcode!");
+            else {//19
+                throw new RuntimeException("No barcode!");//20
             }
-            if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){
-                sum -= 30;
+            if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){//21
+                sum -= 30;//22
             }
         }
-        if (sum <= payment){
-            return true;
+        if (sum <= payment){//23
+            return true;//24
         }
-        else {
-            return false;
+        else {//25
+            return false;//26
         }
-    }
+    }//27
 }
--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,4 +1,49 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SILab2Test {
+
+    @Test
+    public void everyBranch(){
+        List<Item> items1 = new ArrayList<>();
+        items1.add(new Item("Vino","0123",500,20));
+        items1.add(new Item("Cokolado","3456",60,30));
+        items1.add(new Item("Jogurt","1555",85,0));
+        int payment1 = 20000;
+        assertTrue(SILab2.checkCart(items1,payment1));
+
+        List<Item> items2 = new ArrayList<>();
+        items2.add(new Item("Cokolado","1555",150,2));
+        items2.add(new Item("Zejtin","1111",80,2));
+        int payment2 = 100;
+        assertFalse(SILab2.checkCart(items2,payment2));
+
+        //allItems list can't be null!
+        List<Item> items3 = null;
+        RuntimeException exception1 = assertThrows(RuntimeException.class, () ->{
+            SILab2.checkCart(items3,1000);
+        });
+        assertEquals("allItems list can't be null!",exception1.getMessage());
+
+        //Invalid character in item barcode!
+        List<Item> items4 = new ArrayList<>();
+        items4.add(new Item("Sok","a256",35,5));
+        RuntimeException exception2 = assertThrows(RuntimeException.class, () ->{
+            SILab2.checkCart(items4,500);
+        });
+        assertEquals("Invalid character in item barcode!",exception2.getMessage());
+
+        //No barcode!
+        List<Item> items5 = new ArrayList<>();
+        items5.add(new Item("Riba",null,500,2));
+        RuntimeException exception3 = assertThrows(RuntimeException.class, () ->{
+            SILab2.checkCart(items5,1500);
+        });
+        assertEquals("No barcode!",exception3.getMessage());
+    }
+
 }
